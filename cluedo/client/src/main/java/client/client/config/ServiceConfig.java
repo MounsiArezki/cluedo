70a32d15d0816e64089f4dbf27c559444c252e75
matthieu <matthieu.burille-meret@etu.univ-orleans.fr>
2020-03-17 15:16:17 +0100

    changement client config url

--- cluedo/client/src/main/java/client/client/config/ServiceConfig.java
+++ cluedo/client/src/main/java/client/client/config/ServiceConfig.java
@@ -6,7 +6,7 @@ public class ServiceConfig {
     // USER
     //
     public static String URL_USER="/user";
-    public static String URL_USER_CONNEXION="/connexion";
+    public static String URL_USER_CONNEXION=URL_USER+"/connexion";
     public static String URL_USER_ID=URL_USER+"/{id}";
     public static String URL_USER_ID_INVITATION_RECU=URL_USER_ID+"/invitation/recue";
     public static String URL_USER_ID_INVITATION_EMISE=URL_USER_ID+"/invitation/emise";

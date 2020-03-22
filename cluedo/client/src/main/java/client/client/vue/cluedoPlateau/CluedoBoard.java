7627cab6a99ce1d74036c3f2772b84f22615bcd1
arezki <arezki2015@gmail.com>
2020-03-22 12:38:51 +0100

    initialise initialize grid v2

--- cluedo/client/src/main/java/client/client/vue/cluedoPlateau/CluedoBoard.java
+++ cluedo/client/src/main/java/client/client/vue/cluedoPlateau/CluedoBoard.java
@@ -1,15 +1,10 @@
 package client.client.vue.cluedoPlateau;
-
-
-import client.client.modele.entite.Lieu;
 import client.client.modele.entite.io.TextStream;
 import client.client.vue.cluedoPlateau.Key.DirectionKey;
 import client.client.vue.cluedoPlateau.Key.PlaceKey;
 import client.client.vue.place.BasicPlace;
 import client.client.vue.place.DepartPlace;
 import client.client.vue.place.Place;
-import client.client.vue.place.TeleportPlace;
-import javafx.geometry.Insets;
 
 import java.io.InputStream;
 import java.util.Scanner;
@@ -24,14 +19,12 @@ public class CluedoBoard extends Board<Place> {
         super();
         this.setWidth(750);
         this.setHeight(620);
-
-
         initializeGrid();
         draw();
 
 
     }
-
+/*
     private void initializeGrid() {
         String[] map = TextStream.PARSE.apply(TextStream.CLUEDOBOARD.getInputStream());
         super.grid = new Place[map.length][map[0].length()/2];
@@ -48,6 +41,41 @@ public class CluedoBoard extends Board<Place> {
         }
 
     }
+    */
+
+    private void initializeGrid() {
+        InputStream mapStream = TextStream.CLUEDOBOARD.getInputStream();
+        Scanner s = new Scanner(mapStream).useDelimiter("\\A");
+        String rawMap = s.hasNext() ? s.next() : "";
+        String[] map = rawMap.split("\\r?\\n");
+
+        super.grid = new Place[map.length][map[0].length()/2];
+
+        int y = 0;
+        for(String line : map) {
+            int x = -1;
+            for(int i = 0; i < line.length(); i += 2) {
+                x+=1;
+                switch(PlaceKey.getPlace(line.charAt(i))) {
+                    case PATH:
+                        super.grid[y][x] = new BasicPlace();
+                        break;
+                    case UNREACHABLE:
+                        super.grid[y][x] = new Place(DirectionKey.ALL, false, 1);
+                        break;
+                    case START:
+                        super.grid[y][x] = new DepartPlace();
+                        break;
+                }
+
+                if(PlaceKey.getPlace(line.charAt(i)) != PlaceKey.OTHER)
+                    continue;
+
+            }
+            y+=1;
+        }
+    }
+
 
 
 

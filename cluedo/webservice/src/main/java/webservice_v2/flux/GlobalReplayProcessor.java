cf45d4e8f363e165dfb6513b29271f0ee3c9f373
matthieu <matthieu.burille-meret@etu.univ-orleans.fr>
2020-04-08 12:17:17 +0200

    class flux

---
+++ cluedo/webservice/src/main/java/webservice_v2/flux/GlobalReplayProcessor.java
@@ -0,0 +1,23 @@
+package webservice_v2.flux;
+
+import reactor.core.publisher.ReplayProcessor;
+import webservice_v2.modele.entite.Invitation;
+import webservice_v2.modele.entite.Joueur;
+import webservice_v2.modele.entite.Partie;
+import webservice_v2.modele.entite.carte.ICarte;
+
+import java.util.List;
+import java.util.Map;
+
+public class GlobalReplayProcessor {
+
+    public static ReplayProcessor<String> testNotifications = ReplayProcessor.create(0, false);
+
+    public static ReplayProcessor<Partie> partieNotification = ReplayProcessor.create(0, false);
+
+    public static ReplayProcessor<Invitation> invitationsNotification = ReplayProcessor.create(0,false);
+
+    public static ReplayProcessor<Map<ICarte, Joueur>> ficheEnqueteNotification = ReplayProcessor.create(0,false);
+
+    public static ReplayProcessor<List<ICarte>> cartesJoueurNotification = ReplayProcessor.create(0,false);
+}
